e02e6256e0c348554939dbbf43f3c6031c3b7139
Dave Brophy <dave@brophy.uk>
2018-04-29 15:54:34 +0200

    Add readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Jstest
+
+Very simple test. See [main.go](main.go) for code.
--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"fmt"
+
 	"honnef.co/go/js/dom"
 )
 
 func main() {
 	body := dom.GetWindow().Document().GetElementsByTagName("body")[0]
-	body.SetInnerHTML("Hello, World!")
+	body.SetInnerHTML("Hello, HTML!")
+	fmt.Println("Hello, console!")
 }
